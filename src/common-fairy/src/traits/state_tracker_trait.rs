060461c2698adb1be8462c85632997be3deed3b2
Ziad Elshahawy <ziadelshahawygit@gmail.com>
2025-05-26 16:50:00 -0500

    removed dups

--- src/common-fairy/src/traits/state_tracker_trait.rs
+++
@@ -1,6 +0,0 @@
-use crate::prelude::*;
-
-pub trait StateTrackerTrait {
-    fn set_ts(&self, c_id: ContainerId, ts: LogicalTimeStamp);
-    fn get_ts(&self, c_id: &ContainerId) -> (LogicalTimeStamp, LogicalTimeStamp);
-}
